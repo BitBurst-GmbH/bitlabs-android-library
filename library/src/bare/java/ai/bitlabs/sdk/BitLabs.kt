47ecce65ec1ed66b7a09b032cb2a4b3c431cafdc
Omar Raad <ord@Omars-Mac.local>
2022-03-30 12:01:09 +0200

    Create two variants
    - The bare variant is the normal variant that is used natively with Android.
    
    - The unity variant is used with Unity.
    
    - Make BitLabs specific for each variant.
    
    Related issues: BLD-421

--- library/src/bare/java/ai/bitlabs/sdk/BitLabs.kt
+++ library/src/bare/java/ai/bitlabs/sdk/BitLabs.kt
@@ -9,7 +9,6 @@ import ai.bitlabs.sdk.util.TAG
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.unity3d.player.UnityPlayer
 
 /**
  * The main class including all the library functions to use in your code.
@@ -54,24 +53,9 @@ object BitLabs {
         bitLabsRepo?.checkSurveys(onResponseListener)
     }
 
-    /** Don't use this method, use [BitLabs.checkSurveys] instead. */
-    fun checkSurveys(gameObject: String) = checkSurveys { hasSurveys ->
-        if (hasSurveys != null)
-            UnityPlayer.UnitySendMessage(
-                gameObject,
-                "BitLabs - checkSurveys",
-                if (hasSurveys) "Found Surveys!" else "No Surveys!"
-            )
-    }
-
     /** Registers an [OnRewardListener] callback to be invoked when a Survey is completed by the user. */
     fun setOnRewardListener(onRewardListener: OnRewardListener) {
         this.onRewardListener = onRewardListener
-    }
-
-    /** Don't use this method, use [BitLabs.setOnRewardListener] instead */
-    fun setOnRewardListener(gameObject: String) = setOnRewardListener { payout ->
-        UnityPlayer.UnitySendMessage(gameObject, "BitLabs - OnReward", payout.toString())
     }
 
     /** Adds a new tag([key]:[value] pair) to [BitLabs.tags] */
@@ -85,11 +69,10 @@ object BitLabs {
      * It's recommended that that you use a context you know the lifecycle of
      * in order to avoid memory leaks and other issues associated with Activities.
      */
-    @JvmOverloads
-    fun launchOfferWall(context: Context, sdk: String = "NATIVE") = ifInitialised {
+    fun launchOfferWall(context: Context) = ifInitialised {
         with(Intent(context, WebActivity::class.java)) {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
-            putExtra(BUNDLE_KEY_PARAMS, WebActivityParams(token, uid, sdk, tags).url)
+            putExtra(BUNDLE_KEY_PARAMS, WebActivityParams(token, uid, tags).url)
             context.startActivity(this)
         }
     }

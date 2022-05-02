ba02053d1b685646bc3af71d0da721281f9e73b7
Omar Raad <ord@Omars-Mac.fritz.box>
2022-05-02 12:07:36 +0200

    v2.2.0

--- library/src/main/java/ai/bitlabs/sdk/data/model/Survey.kt
+++ library/src/main/java/ai/bitlabs/sdk/data/model/Survey.kt
@@ -7,6 +7,19 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+/**
+ * Represents the survey the user can take.
+ * @property[networkId]
+ * @property[id] The id of the Survey.
+ * @property[cpi] CPI of this survey in USD without any formatting applied.
+ * @property[value] CPI formatted according to your app settings. Can be shown to the user directly.
+ * @property[loi] Assumed length of the survey in minutes
+ * @property[remaining] Amount of users that can still open the survey
+ * @property[details] See [Details].
+ * @property[rating] Difficulty ranking of this survey. 1-5 (1 = hard, 5 = easy). Minimum value is 1, maximum value is 5.
+ * @property[link] This link can be used as is to open the survey. All relevant details are inserted on the server.
+ * @property[missingQuestions] The amount of questions that have to be answered before the survey is guaranteed to be openable by the user.
+ */
 data class Survey(
     @Json(name = "network_id") val networkId: Int,
     val id: Int,

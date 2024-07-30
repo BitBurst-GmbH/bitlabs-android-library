34c30bd9f8a1f6a3c1a0a5a58c0d5b6ba3d2f1b8
Omar <omar.raad@prodege.com>
2024-07-30 13:03:18 +0200

    Some Changes

--- library/src/androidTest/java/ai/bitlabs/sdk/views/WebActivityTest.kt
+++ library/src/androidTest/java/ai/bitlabs/sdk/views/WebActivityTest.kt
@@ -9,12 +9,18 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.view.View
 import android.webkit.WebView
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -23,155 +29,173 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockkObject
 import io.mockk.verify
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Test
+import org.junit.runner.RunWith
 
 private const val TOKEN = BuildConfig.APP_TOKEN
 private const val UID = "diffindocongress"
 
+@RunWith(AndroidJUnit4::class)
 class WebActivityTest {
 
     private val context = ApplicationProvider.getApplicationContext<Context>()
 
-//    @Test
-//    fun urlExtra_No_BUNDLE_KEY_URL_DestroyActivity() {
-//        ActivityScenario.launch(WebActivity::class.java).use {
-//            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
-//        }
-//    }
-
-//    @Test
-//    fun urlExtra_BUNDLE_KEY_URL_EmptyString_DestroyActivity() {
-//        val intent = TestUtils.createWebActivityIntent("")
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
-//        }
-//    }
-
-//    @Test
-//    fun urlExtra_BUNDLE_KEY_URL_NotString_DestroyActivity() {
-//        val intent = Intent(context, WebActivity::class.java).apply {
-//            putExtra(BUNDLE_KEY_URL, 123)
-//        }
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
-//        }
-//    }
-//
-//    @Test
-//    fun urlExtra_BUNDLE_KEY_URL_CorrectURLString_CreateActivity() {
-//        val url = "https://www.google.com"
-//        val intent = TestUtils.createWebActivityIntent(url)
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            assertThat(it.state).isEqualTo(Lifecycle.State.RESUMED)
-//        }
-//    }
-//
-//    @Test
-//    fun colorExtra_No_BUNDLE_KEY_COLOR_WhiteToolbarBackground() {
-//        // Create a WebActivity with non-OfferWall URL to show the toolbar
-//        val url = "https://www.google.com"
-//        val intent = TestUtils.createWebActivityIntent(url)
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            onView(withId(R.id.toolbar_bitlabs)).check { view, _ ->
-//                assertThat(view).isInstanceOf(Toolbar::class.java)
-//                val gradient = view.background as GradientDrawable
-//                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun colorExtra_BUNDLE_KEY_COLOR_Empty_WhiteToolbarBackground() {
-//        // Create a WebActivity with non-OfferWall URL to show the toolbar
-//        val url = "https://www.google.com"
-//        val intent = TestUtils.createWebActivityIntent(url, intArrayOf())
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            onView(withId(R.id.toolbar_bitlabs)).check { view, _ ->
-//                assertThat(view).isInstanceOf(Toolbar::class.java)
-//                val gradient = view.background as GradientDrawable
-//                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun colorExtra_BUNDLE_KEY_COLOR_NotIntArrayOf_WhiteToolbarBackground() {
-//        // Create a WebActivity with non-OfferWall URL to show the toolbar
-//        val url = "https://www.google.com"
-//        val intent = Intent(context, WebActivity::class.java).apply {
-//            putExtra(BUNDLE_KEY_URL, url)
-//            putExtra(BUNDLE_KEY_COLOR, 123)
-//        }
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            onView(withId(R.id.toolbar_bitlabs)).check { view, _ ->
-//                assertThat(view).isInstanceOf(Toolbar::class.java)
-//                val gradient = view.background as GradientDrawable
-//                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun colorExtra_BUNDLE_KEY_COLOR_CorrectIntArray_CorrectToolbarBackground() {
-//        val colors = intArrayOf(123, 123)
-//        val url = "https://www.google.com"
-//        val intent = TestUtils.createWebActivityIntent(url, colors)
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            onView(withId(R.id.toolbar_bitlabs)).check { view, _ ->
-//                assertThat(view).isInstanceOf(Toolbar::class.java)
-//                val gradient = view.background as GradientDrawable
-//                assertThat(gradient.colors).isEqualTo(colors)
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun toolbar_PageIsBitlabsOfferwall_IsNotDisplayed() {
-//        val url = "https://web.bitlabs.ai"
-//        val intent = TestUtils.createWebActivityIntent(url)
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            onView(withId(R.id.toolbar_bitlabs)).check(matches(not(isDisplayed())))
-//        }
-//    }
-//
-//    @ TODO: Fix this test
-//    @Test
-//    fun toolbar_PageIsNotOfferwall_IsDisplayed() {
-//        val url = "https://www.google.com"
-//        val intent = TestUtils.createWebActivityIntent(url)
-//
-//        ActivityScenario.launch<WebActivity>(intent).use {
-//            Thread.sleep(1000)
-//            onView(withId(R.id.toolbar_bitlabs)).check(matches(isDisplayed()))
-//        }
-//    }
-//
     @Test
-    fun onBackPressed_PageIsNotOfferwall_ShowLeaveSurveyDialog() {
+    fun urlExtra_No_BUNDLE_KEY_URL_DestroyActivity() {
+        ActivityScenario.launch(WebActivity::class.java).use {
+            Thread.sleep(2000)
+            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun urlExtra_BUNDLE_KEY_URL_EmptyString_DestroyActivity() {
+        val intent = TestUtils.createWebActivityIntent("")
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun urlExtra_BUNDLE_KEY_URL_NotString_DestroyActivity() {
+        val intent = Intent(context, WebActivity::class.java).apply {
+            putExtra(BUNDLE_KEY_URL, 123)
+        }
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            assertThat(it.state).isEqualTo(Lifecycle.State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun urlExtra_BUNDLE_KEY_URL_CorrectURLString_CreateActivity() {
         val url = "https://www.google.com"
         val intent = TestUtils.createWebActivityIntent(url)
 
         ActivityScenario.launch<WebActivity>(intent).use {
-            Thread.sleep(500)
-            onView(isRoot()).perform(pressBack())
-            onView(withId(androidx.appcompat.R.id.alertTitle)).inRoot(isDialog())
-                .check(matches(isDisplayed()))
+            Thread.sleep(2000)
+            assertThat(it.state).isEqualTo(Lifecycle.State.RESUMED)
         }
     }
+
+    @Test
+    fun colorExtra_No_BUNDLE_KEY_COLOR_WhiteToolbarBackground() {
+        // Create a WebActivity with non-OfferWall URL to show the toolbar
+        val url = "https://www.google.com"
+        val intent = TestUtils.createWebActivityIntent(url)
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            it.onActivity { activity ->
+                val toolbar = activity.findViewById<Toolbar>(R.id.toolbar_bitlabs)
+                val gradient = toolbar.background as GradientDrawable
+                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
+            }
+        }
+    }
+
+    @Test
+    fun colorExtra_BUNDLE_KEY_COLOR_Empty_WhiteToolbarBackground() {
+        // Create a WebActivity with non-OfferWall URL to show the toolbar
+        val url = "https://www.google.com"
+        val intent = TestUtils.createWebActivityIntent(url, intArrayOf())
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            it.onActivity { activity ->
+                val toolbar = activity.findViewById<Toolbar>(R.id.toolbar_bitlabs)
+                val gradient = toolbar.background as GradientDrawable
+                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
+            }
+        }
+    }
+
+    @Test
+    fun colorExtra_BUNDLE_KEY_COLOR_NotIntArrayOf_WhiteToolbarBackground() {
+        // Create a WebActivity with non-OfferWall URL to show the toolbar
+        val url = "https://www.google.com"
+        val intent = Intent(context, WebActivity::class.java).apply {
+            putExtra(BUNDLE_KEY_URL, url)
+            putExtra(BUNDLE_KEY_COLOR, 123)
+        }
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            it.onActivity { activity ->
+                val toolbar = activity.findViewById<Toolbar>(R.id.toolbar_bitlabs)
+                val gradient = toolbar.background as GradientDrawable
+                assertThat(gradient.colors).isEqualTo(intArrayOf(Color.WHITE, Color.WHITE))
+            }
+        }
+    }
+
+    @Test
+    fun colorExtra_BUNDLE_KEY_COLOR_CorrectIntArray_CorrectToolbarBackground() {
+        val colors = intArrayOf(123, 123)
+        val url = "https://www.google.com"
+        val intent = TestUtils.createWebActivityIntent(url, colors)
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            it.onActivity { activity ->
+                val toolbar = activity.findViewById<Toolbar>(R.id.toolbar_bitlabs)
+                val gradient = toolbar.background as GradientDrawable
+                assertThat(gradient.colors).isEqualTo(colors)
+            }
+        }
+    }
+
+    @Test
+    fun toolbar_PageIsBitlabsOfferwall_IsNotDisplayed() {
+        val url = "https://web.bitlabs.ai"
+        val intent = TestUtils.createWebActivityIntent(url)
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(2000)
+            onView(withId(R.id.toolbar_bitlabs)).check(matches(not(isDisplayed())))
+        }
+    }
+
+    @Test
+    fun toolbar_PageIsNotOfferwall_IsDisplayed() {
+        val url = "https://www.google.com"
+        val intent = TestUtils.createWebActivityIntent(url)
+
+        ActivityScenario.launch<WebActivity>(intent).use {
+            Thread.sleep(6000)
+            onView(withId(R.id.toolbar_bitlabs)).check(matches(isDisplayed()))
+        }
+    }
+
+//    @Test
+//    fun onBackPressed_PageIsNotOfferwall_ShowLeaveSurveyDialog() {
+//        val url = "https://www.google.com"
+//        val intent = TestUtils.createWebActivityIntent(url)
 //
+//        ActivityScenario.launch<WebActivity>(intent).use {
+//            Thread.sleep(4000)
+//            onView(isRoot()).perform(pressBack())
+//
+//            Thread.sleep(4000)
+//            onView(withId(androidx.appcompat.R.id.alertTitle)).inRoot(isDialog())
+//                .check(matches(isDisplayed()))
+//        }
+//    }
+
 //    @Test
 //    fun leaveSurveyDialog_AnyOptionClicked_LeaveSurveyCalled() {
 //        val url = "https://www.google.com"
@@ -189,51 +213,56 @@ class WebActivityTest {
 //            every { BitLabs.leaveSurvey(any(), any()) } returns Unit
 //
 //            ActivityScenario.launch<WebActivity>(intent).use {
-//                Thread.sleep(500)
+//                Thread.sleep(10000)
 //
 //                it.onActivity { activity ->
 //                    activity.findViewById<WebView>(R.id.wv_bitlabs).evaluateJavascript(jsCode) {}
 //                }
 //
-//                Thread.sleep(500)
+//                Thread.sleep(10000)
 //
 //                onView(isRoot()).perform(pressBack())
+//                Thread.sleep(10000)
 //
 //                onView(withText(R.string.leave_reason_other)).inRoot(isDialog())
 //                    .check(matches(isDisplayed())).perform(click())
 //
 //                verify(exactly = 1) { BitLabs.leaveSurvey(any(), any()) }
 //
-//                Thread.sleep(400)
+//                Thread.sleep(10000)
 //
 //                onView(isRoot()).perform(pressBack())
+//                Thread.sleep(10000)
 //
 //                onView(withText(R.string.leave_reason_sensitive)).inRoot(isDialog())
 //                    .check(matches(isDisplayed())).perform(click())
 //
 //                verify(exactly = 2) { BitLabs.leaveSurvey(any(), any()) }
 //
-//                Thread.sleep(400)
+//                Thread.sleep(10000)
 //
 //                onView(isRoot()).perform(pressBack())
+//                Thread.sleep(10000)
 //
 //                onView(withText(R.string.leave_reason_technical)).inRoot(isDialog())
 //                    .check(matches(isDisplayed())).perform(click())
 //
 //                verify(exactly = 3) { BitLabs.leaveSurvey(any(), any()) }
 //
-//                Thread.sleep(400)
+//                Thread.sleep(10000)
 //
 //                onView(isRoot()).perform(pressBack())
+//                Thread.sleep(10000)
 //
 //                onView(withText(R.string.leave_reason_uninteresting)).inRoot(isDialog())
 //                    .check(matches(isDisplayed())).perform(click())
 //
 //                verify(exactly = 4) { BitLabs.leaveSurvey(any(), any()) }
 //
-//                Thread.sleep(400)
+//                Thread.sleep(10000)
 //
 //                onView(isRoot()).perform(pressBack())
+//                Thread.sleep(10000)
 //
 //                onView(withText(R.string.leave_reason_too_long)).inRoot(isDialog())
 //                    .check(matches(isDisplayed())).perform(click())
